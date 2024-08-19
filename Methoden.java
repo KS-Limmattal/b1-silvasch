30cda8170a07286432fa9098aa815e43a3ed164d
Silvan Schmidt <mail@silvanschmidt.ch>
2024-08-19 14:56:39 +0200

    hours

--- Methoden.java
+++ Methoden.java
@@ -1,13 +1,26 @@
 public class Methoden {
 
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
+
+    public static String hours(int seconds) {
+        int restAfterHours = seconds % 3600;
+        int hours = (seconds - restAfterHours) / 3600;
+        int restAfterMinutes = restAfterHours % 60;
+        int minutes = (restAfterHours - restAfterMinutes) / 60;
+        return java.lang.String.format("%02d:%02d:%02d", hours, minutes, restAfterMinutes);
+    }
     
     public static void main(String[] args){
         // Test-Code für Teilaufgabe a)
         System.out.println("Aufgabe a)");
         System.out.println(hours(3));
         System.out.println(hours(75));
-        // TODO: Schreibe mehr Testcode
+        System.out.println(hours(0));
+        System.out.println(hours(59));
+        System.out.println(hours(60));
+        System.out.println(hours(100));
+        System.out.println(hours(3600));
+        System.out.println(hours(4000));
         System.out.println();
 
         // Test-Code für Teilaufgabe b)
