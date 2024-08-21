6cbcc1fa161e0016b713cac3850433c866327625
Silvan Schmidt <mail@silvanschmidt.ch>
2024-08-21 07:49:19 +0200

    better primes

--- Loops.java
+++ Loops.java
@@ -1,67 +1,55 @@
-import java.util.ArrayList;
-
 public class Loops {
     
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
 
-    static ArrayList<Integer> generatePrimes() {
-        ArrayList<Integer> primes = new ArrayList<Integer>();
-        primes.add(2);
-        primes.add(3);
-        primes.add(5);
-        primes.add(7);
-        primes.add(11);
-        primes.add(13);
-        primes.add(17);
-        primes.add(19);
-        primes.add(23);
-        primes.add(29);
-        primes.add(31);
-        primes.add(37);
-        primes.add(41);
-        primes.add(43);
-        primes.add(47);
-        primes.add(53);
-        primes.add(59);
-        primes.add(61);
-        primes.add(67);
-        primes.add(71);
-        primes.add(73);
-        primes.add(79);
-        primes.add(83);
-        primes.add(89);
-        primes.add(97);
-        return primes;
+    static boolean isPrime(int num) {
+        if (num == 0 || num == 1) {
+            return false;
+        }
+
+        if (num == 2) {
+            return true;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void primeFactorisation(int n) {
-        System.out.print(java.lang.String.format("Prime facorisation of %d = ", n));
+        int originalN = n;
         
-        ArrayList<Integer> primes = generatePrimes();
-        ArrayList<Integer> factors = new ArrayList<Integer>();
+        System.out.print(java.lang.String.format("Prime facorisation of %d = ", n));
 
-        int currentFactor = primes.remove(0);
-
-        while (true) {
-            if (n == 1) {
-                break;
+        boolean isFirstFactor = true;
+        
+        outer:
+        for (int prime = 2; prime <= originalN; prime += 1) {
+            if (!isPrime(prime)) {
+                continue;
             }
-            
-            if (n % currentFactor == 0) {
-                n = n / currentFactor;
-                factors.add(currentFactor);
-            } else {
-                currentFactor = primes.remove(0);
+
+            while (true) {
+                if (n == 1) {
+                    break outer;
+                }
+                if (n % prime == 0) {
+                    n = n / prime;
+                    if (isFirstFactor) {
+                        System.out.print(prime);
+                        isFirstFactor = false;
+                    } else {
+                        System.out.print(" * " + Integer.toString(prime));
+                    }
+                } else {
+                    break;
+                }
             }
         }
-
-        int firstFactor = factors.remove(0);
-        System.out.print(firstFactor);
-
-        for (int i = 0; i < factors.size(); i++) {
-            System.out.print(java.lang.String.format(" * %d", factors.get(i)));
-        }
-
         System.out.println();
     }
     
@@ -70,6 +58,20 @@ public class Loops {
         // Test-Code für Teilaufgabe c)
         System.out.println("Aufgabe c)");
         primeFactorisation(48);
+        primeFactorisation(541);
+        primeFactorisation(72);
+        primeFactorisation(36);
+        primeFactorisation(48);
+        primeFactorisation(12);
+        primeFactorisation(100);
+        primeFactorisation(84);
+        primeFactorisation(8);
+        primeFactorisation(32);
+        primeFactorisation(24);
+        primeFactorisation(91);
+        primeFactorisation(15);
+
+        
         // TODO: Schreibe mehr Testcode
         System.out.println();
 
